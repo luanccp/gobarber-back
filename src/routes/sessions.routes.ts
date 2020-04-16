0097172807198e8f2988c49650c3b9b02bbfe488
Luan Castheloge <luanccp@gmail.com>
2020-04-16 11:04:59 -0300

    authenticate done

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,29 @@
+import express, { Router } from 'express'
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+
+const sessionsRouter = Router()
+
+sessionsRouter.use(express.json())
+
+
+sessionsRouter.post("/", async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.execute({
+      email,
+      password,
+    })
+
+    delete user.password;
+
+    return response.json({ user })
+  } catch (err) {
+    return response.status(400).json({ message: err.message })
+  }
+});
+
+export default sessionsRouter
