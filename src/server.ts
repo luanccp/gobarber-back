5ee2bcb2ff39d0b926e56b1e4ecc1c35a9a512aa
Luan Castheloge <luanccp@gmail.com>
2020-04-24 08:56:17 -0300

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors'
+import cors from 'cors';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload'
 
@@ -10,7 +12,8 @@ import AppError from './errors/AppError';
 
 const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.use('/files', express.static(uploadConfig.directory))
 app.use(routes)
