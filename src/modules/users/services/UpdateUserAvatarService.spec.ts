29193e65220eb492811f847721072eca9abf60c0
Luan Castheloge <luanccp@gmail.com>
2020-05-19 16:13:25 -0300

    refactor: add beforeEach on tests

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -4,13 +4,18 @@ import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import UpdateUserAvatarService from './UpdateUserAvatarService';
 import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
 
+let fakeUsersRepository: FakeUsersRepository;
+let fakeStorageProvider: FakeStorageProvider;
+let updateUserAvatar: UpdateUserAvatarService;
+
 describe('UploadUserAvatar', () => {
+
+  beforeEach(()=> {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeStorageProvider = new FakeStorageProvider();
+    updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
+  })
   it('should be able to update avatar', async () => {
-
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-    const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
-
     const user = await fakeUsersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
@@ -20,17 +25,10 @@ describe('UploadUserAvatar', () => {
       user_id: user.id,
       avatarFilename: 'AvatarDoTest.jpg'
     })
-
     expect(user.avatar).toBe('AvatarDoTest.jpg');
   });
 
   it('should not be able to update avatar from non exist user', async () => {
-
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-    const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
-
-
     await expect(updateUserAvatar.execute({
       user_id: 'non-exist-user',
       avatarFilename: 'AvatarDoTest.jpg'
@@ -38,13 +36,7 @@ describe('UploadUserAvatar', () => {
   });
 
   it('should delete old avatar when updating new one', async () => {
-
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-
     const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile')
-
-    const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
 
     const user = await fakeUsersRepository.create({
       name: 'John Doe',
