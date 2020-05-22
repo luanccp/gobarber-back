e7f72d4217d1d3ad99d889c11379ac548853b95f
Luan Castheloge <luanccp@gmail.com>
2020-05-22 11:40:14 -0300

    feat: display all day and month availability

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,13 +2,18 @@ import express, { Router } from 'express'
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
 
 const providersRouter = Router()
 const providersController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
-providersRouter.use(express.json());
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get("/",providersController.index);
+providersRouter.get("/:provider_id/month-availability",providerMonthAvailabilityController.index);
+providersRouter.get("/:provider_id/day-availability",providerDayAvailabilityController.index);
 
 export default providersRouter
