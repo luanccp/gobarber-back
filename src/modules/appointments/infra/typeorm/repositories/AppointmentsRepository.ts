768a6745997d934398c9bfd4e65f4b27d4b00c9e
Luan Castheloge <luanccp@gmail.com>
2020-06-01 11:33:29 -0300

    refactor: adding user data at appointment data

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -58,7 +58,8 @@ class AppointmentsRepository implements IAppointmentsRepository {
         date: Raw(dateFieldName =>
           `to_char(${dateFieldName}, 'DD-MM-YYYY') ='${parsedDay}-${parsedMonth}-${year}'`
         )
-      }
+      },
+      relations: ['user'],
     })
 
     return appointments;
