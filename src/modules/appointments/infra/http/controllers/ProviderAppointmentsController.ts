0ab8bbbefa264ac3d1a679cacd38d89248dbc5ff
Luan Castheloge <luanccp@gmail.com>
2020-06-01 10:49:06 -0300

    refactor: change to query paramns for frontend app

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -9,16 +9,16 @@ export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
 
-    const { day, year, month } = request.body;
+    const { day, year, month } = request.query;
 
 
     const listProviderAppointments = container.resolve(ListProviderAppointmentsService);
 
     const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      year,
-      month
+      month: Number(month),
+      year: Number(year),
+      day: Number(day)
     })
 
     return response.json(appointments);
